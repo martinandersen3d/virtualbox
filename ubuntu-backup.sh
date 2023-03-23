b22f620c3cd2b13d210f01f51731af19cb1129cc
Martin andersen[D[D[D[D[D[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[C <martinandersen3d@gmail.com>
2023-03-23 01:14:58 +0100

    init

---
+++ ubuntu-backup.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# TODO: take params
+# echo "The first parameter is: $1"
+
+VM_NAME="VM_Ubuntu"
+
+# Send Shutdown signal - os will shutdown nicely
+VBoxManage controlvm VM_Ubuntu acpipowerbutton
+
+# Wait until vm is shut down
+while true
+do
+  if VBoxManage showvminfo "$VM_NAME" | grep -q "State:.*running"; then
+    echo "$VM_NAME is running"
+  else
+    echo "$VM_NAME is not running"
+    # Do something here, like starting the virtual machine
+    break
+  fi
+  sleep 5
+done
+
+echo "Continue script..."
+
+mv /home/h/VM/Exports/VM_Ubuntu.ova /home/h/VM/Exports/VM_Ubuntu.ova
+
+VBoxManage export VM_Ubuntu -o /home/h/VM/Exports/VM_Ubuntu_1.ova
+
+sleep 10
+
+VBoxManage startvm VM_Ubuntu
