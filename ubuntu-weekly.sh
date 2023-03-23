96e987d6e2448d872b2fcec2c9149c9103f34e50
Martin andersen[D[D[D[D[D[D[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[C <martinandersen3d@gmail.com>
2023-03-23 20:45:41 +0100

    cron mod

--- ubuntu-weekly.sh
+++ ubuntu-weekly.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+
+# This script will run from CRON
+
+# Setup Environment:
+
+# Set the DISPLAY variable to the current user's display
+export DISPLAY=:0
+
+# Set the DBUS_SESSION_BUS_ADDRESS variable to the current user's DBus session address
+export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus
+
+# Set the XAUTHORITY variable to the current user's Xauthority file
+export XAUTHORITY=/home/h/.Xauthority                # <<<<<<<<<<<<< CHANGE USERNAME  
+
+#---------------------------------------------
+# Script you want to run:
+#---------------------------------------------
+
+
 /home/h/Scripts/virtualbox/ubuntu-backup.sh "weekly"
